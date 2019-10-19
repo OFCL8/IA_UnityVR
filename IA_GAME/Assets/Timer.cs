7b78c7d84afee509ea6bf3741b5f0bcd779b434e
MidGsus <55570667+MidGsus@users.noreply.github.com>
2019-10-19 13:48:05 -0700

    Added all the figures into scene, fixed the instruction and programed the scene reset with the cap of 3 maximum retrys

---
+++ IA_GAME/Assets/Timer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public static Timer Instance;
+
+    float duracionDelNivel =5;
+    public int intentosFallidos = 0;
+    public float timecurrent;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        duracionDelNivel = 5;
+        timecurrent = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            timecurrent += Time.deltaTime;
+            //Debug.Log(duracionDelNivel);
+            if (timecurrent >= duracionDelNivel)
+            {
+
+                //Perdiste
+                intentosFallidos++;
+                timecurrent = 0;
+                ReloadLevel();
+            }
+               
+    }
+    public void ReloadLevel()
+    {
+        if (intentosFallidos >=3)
+        {
+            //perdiste
+            Destroy(this);
+        }
+        else
+        {
+            SceneManager.LoadScene("Nivel1");
+        }             
+    }
+}
