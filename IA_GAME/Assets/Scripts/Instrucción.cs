a58b7648177505011ea9be03fdbfe1ad91fc2636
OFCL8 <49254138+OFCL8@users.noreply.github.com>
2019-10-19 01:00:02 -0700

    Intrucciones nivel 1

---
+++ IA_GAME/Assets/Scripts/Instrucción.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instrucción : MonoBehaviour
+{
+    public float TiempoDeAparicion = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TiempoDeAparicion -= Time.deltaTime;
+        if(TiempoDeAparicion<=0)
+        { Destroy(this.gameObject); }
+    }
+}
